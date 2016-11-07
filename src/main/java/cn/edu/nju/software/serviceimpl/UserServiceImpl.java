523ee7380ddfb40086085883a1fd94623c2a7eaf
131250105 <465033064@qq.com>
2016-11-07 20:15:46 +0800

    merge

--- src/main/java/cn/edu/nju/software/serviceimpl/UserServiceImpl.java
+++ src/main/java/cn/edu/nju/software/serviceimpl/UserServiceImpl.java
@@ -1,30 +1,87 @@
 package cn.edu.nju.software.serviceimpl;
 
+import cn.edu.nju.software.dao.UserDao;
 import cn.edu.nju.software.entity.User;
 import cn.edu.nju.software.service.UserService;
 import cn.edu.nju.software.util.ResultDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
  * Created by zy118686 on 2016/11/6.
  */
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService{
-    @Override
-    public ResultDTO<User> saveUser(User user) {
-        return null;
-    }
+
+    @Resource
+    UserDao userDao ;
 
     @Override
-    public ResultDTO<User> updateUser(User user) {
-        return null;
+    public ResultDTO<User> saveorUpdateUser(User user) {
+        ResultDTO<User> resultDTO = new ResultDTO<User>();
+
+        try {
+            User u = userDao.save(user);
+            if(u == null){
+                resultDTO.setSuccess(false);
+                resultDTO.setData(null);
+                resultDTO.setErrorMsg("Unknown error user is null ");
+                log.error("Unknown error user is null");
+                return resultDTO;
+            }
+            resultDTO.setData(u);
+            resultDTO.setSuccess(true);
+        }catch(Exception e){
+            log.error("error in save or update user ",e);
+            resultDTO.setErrorMsg(e.getMessage());
+            resultDTO.setSuccess(false);
+            resultDTO.setData(null);
+        }
+        return resultDTO;
     }
 
     @Override
     public ResultDTO<User> deleteUser(User user) {
-        return null;
+        ResultDTO<User> resultDTO = new ResultDTO<User>();
+        try {
+            userDao.delete(user);
+            resultDTO.setData(null);
+            resultDTO.setSuccess(true);
+        }catch(Exception e){
+            log.error("error in delete user ",e);
+            resultDTO.setErrorMsg(e.getMessage());
+            resultDTO.setSuccess(false);
+            resultDTO.setData(null);
+        }
+        return resultDTO;
+    }
+
+    @Override
+    public ResultDTO<User> queryUserById(String Id) {
+        ResultDTO<User> resultDTO = new ResultDTO<User>();
+
+        try {
+            User u =userDao.findOne(Long.parseLong(Id));
+            if(u==null){
+                log.warn("no such User the Id is "+Id);
+                resultDTO.setSuccess(false);
+                resultDTO.setData(u);
+                resultDTO.setErrorMsg(null);
+                return resultDTO;
+            }
+            resultDTO.setData(u);
+            resultDTO.setSuccess(true);
+        }catch(Exception e){
+            log.error("error in query User by Id the Id is "+Id,e);
+            resultDTO.setErrorMsg(e.getMessage());
+            resultDTO.setSuccess(false);
+            resultDTO.setData(null);
+        }
+        return resultDTO;
     }
 
     @Override
