8483961455d867d263163c0aa9fdf6638878079d
larry.zou <larry.zou@oocl.com>
2016-11-07 11:34:18 +0800

    js extend

--- src/main/webapp/js/application/module/authority/view/LoginView.js
+++ src/main/webapp/js/application/module/authority/view/LoginView.js
@@ -1,61 +1,121 @@
-define(["helper/view/BaseView", "../model/LoginModel", "text!../template/login.tpl", "text!../css/login.css"], function (BaseView, LoginModel, LoginTpl, LoginCss) {
-    var LoginView = BaseView.extend({
-        initialize: function () {
-            this.render();
-            this.model.on("change", this.render, this);
-            // LoginView.prototype.initialize.call(this);
-        },
-        title: '登录',
-        el: 'body',
-        model: new LoginModel,
-        events: {
-            "click #login": "login"
-        },
-        tpl: LoginTpl,
-        css: LoginCss,
-        render: function () {
-            $('title').html(this.title);
-            $('head').append($("<style>" + this.css + "</style>"))
-            $(this.el).html(_.template(this.tpl)(this.model.toJSON()));
-        },
-        login: function (e) {
-            var data = {};
-            $("form.login").serializeArray().map(function (x) {
-                data[x.name] = x.value;
-            });
-            this.model.set(data);
-            var errorMsg = this.verify(data);
-            if (errorMsg) {
-                this.model.set({
-                    errorMsg: errorMsg,
+define(["helper/util", "helper/view/BaseView", "../model/LoginModel", "text!../template/login.tpl", "text!../css/login.css"],
+    function (Util, BaseView, LoginModel, LoginTpl, LoginCss) {
+        var LoginView = Util.extend(BaseView, {
+            initialize: function () {
+                this.render();
+                this.model.on("change", this.render, this);
+                LoginView.superclass.initialize.call(this);
+            },
+            title: '登录',
+            el: 'body',
+            model: new LoginModel,
+            events: {
+                "click #login": "login"
+            },
+            tpl: LoginTpl,
+            css: LoginCss,
+            render: function () {
+                $('title').html(this.title);
+                $('head').append($("<style>" + this.css + "</style>"))
+                $(this.el).html(_.template(this.tpl)(this.model.toJSON()));
+            },
+            login: function (e) {
+                var data = {};
+                $("form.login").serializeArray().map(function (x) {
+                    data[x.name] = x.value;
                 });
-                return;
-            } else {
-                this.model.set({
-                    errorMsg: '',
-                });
+                this.model.set(data);
+                var errorMsg = this.verify(data);
+                if (errorMsg) {
+                    this.model.set({
+                        errorMsg: errorMsg,
+                    });
+                    return;
+                } else {
+                    this.model.set({
+                        errorMsg: '',
+                    });
+                }
+                // $.ajax({
+                //     cache: true,
+                //     type: "POST",
+                //     url: ajaxCallUrl,
+                //     data: $('#yourformid').serialize(),// 你的formid
+                //     async: false,
+                //     error: function (request) {
+                //         alert("Connection error");
+                //     },
+                //     success: function (data) {
+                //         $("#commonLayout_appcreshi").parent().html(data);
+                //     }
+                // });
+                window.location.href = '#teams';
+            },
+            verify: function (data) {
+                if (!data.username || !data.password) {
+                    return '用户名或密码不能为空';
+                }
+                return "";
             }
-            // $.ajax({
-            //     cache: true,
-            //     type: "POST",
-            //     url: ajaxCallUrl,
-            //     data: $('#yourformid').serialize(),// 你的formid
-            //     async: false,
-            //     error: function (request) {
-            //         alert("Connection error");
-            //     },
-            //     success: function (data) {
-            //         $("#commonLayout_appcreshi").parent().html(data);
-            //     }
-            // });
-            window.location.href = '#teams';
-        },
-        verify: function (data) {
-            if (!data.username || !data.password) {
-                return '用户名或密码不能为空';
-            }
-            return "";
-        }
+        });
+
+        // var LoginView = BaseView.extend({
+        //     initialize: function () {
+        //         this.render();
+        //         this.model.on("change", this.render, this);
+        //         // LoginView.prototype.initialize.call(this);
+        //     },
+        //     title: '登录',
+        //     el: 'body',
+        //     model: new LoginModel,
+        //     events: {
+        //         "click #login": "login"
+        //     },
+        //     tpl: LoginTpl,
+        //     css: LoginCss,
+        //     render: function () {
+        //         $('title').html(this.title);
+        //         $('head').append($("<style>" + this.css + "</style>"))
+        //         $(this.el).html(_.template(this.tpl)(this.model.toJSON()));
+        //     },
+        //     login: function (e) {
+        //         var data = {};
+        //         $("form.login").serializeArray().map(function (x) {
+        //             data[x.name] = x.value;
+        //         });
+        //         this.model.set(data);
+        //         var errorMsg = this.verify(data);
+        //         if (errorMsg) {
+        //             this.model.set({
+        //                 errorMsg: errorMsg,
+        //             });
+        //             return;
+        //         } else {
+        //             this.model.set({
+        //                 errorMsg: '',
+        //             });
+        //         }
+        //         // $.ajax({
+        //         //     cache: true,
+        //         //     type: "POST",
+        //         //     url: ajaxCallUrl,
+        //         //     data: $('#yourformid').serialize(),// 你的formid
+        //         //     async: false,
+        //         //     error: function (request) {
+        //         //         alert("Connection error");
+        //         //     },
+        //         //     success: function (data) {
+        //         //         $("#commonLayout_appcreshi").parent().html(data);
+        //         //     }
+        //         // });
+        //         window.location.href = '#teams';
+        //     },
+        //     verify: function (data) {
+        //         if (!data.username || !data.password) {
+        //             return '用户名或密码不能为空';
+        //         }
+        //         return "";
+        //     }
+        // });
+        return LoginView;
     });
-    return LoginView;
-});
--- src/main/webapp/js/helper/util.js
+++ src/main/webapp/js/helper/util.js
@@ -1,3 +1,16 @@
-define([], function() {
-	return "abc";
+define(["underscore"], function (_) {
+    var Util = {
+        extend: function (superclass, destination) {
+            var child = {};
+            child.superclass = superclass;
+            _.extend(child, superclass);
+            _.extend(child, destination || {});
+
+            // var child = destination || {};
+            // child.superclass = superclass;
+            // _.extend(superclass, child);
+            return child;
+        }
+    }
+    return Util;
 });
